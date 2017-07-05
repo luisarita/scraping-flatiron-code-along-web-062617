81f68650e8dddd0f37b51d56baad96aee56945bd
Luis Arita <github@luisarita.com>
2017-07-05 15:44:06 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,23 @@
+class Course
+    attr_accessor :title, :schedule, :description
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    #def schedule
+    #end
+    #def description
+    #end
+
+    def self.reset_all
+        @@all = []
+    end
+    def self.all
+        @@all
+    end
+   
+end
 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -14,6 +14,17 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+  end
+
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)    
+  end
+  def get_courses
+    doc = get_page
+    doc.css(".post")
+  end
+  def make_courses
   end
   
 end
